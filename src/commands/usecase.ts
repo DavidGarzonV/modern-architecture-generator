6d13e87523422f16de054f35a07da3fac53efd39
David Garzón Vélez <dgv241198@gmail.com>
2024-05-08 11:03:31 -0500

    feat: Add context select for use cases

--- src/commands/usecase.ts
+++ src/commands/usecase.ts
@@ -1,12 +1,33 @@
 import { program } from 'commander';
+import { ProjectStructure } from 'lib/shared/project-structure';
 import CreateUseCase from 'lib/usecase';
 import prompts from 'prompts';
+import { pathExists, readDirectory } from 'utils/file';
+
+const getContexts = async (): Promise<prompts.Choice[]> => {
+	const projectStructure = new ProjectStructure();
+	const useCasesFolder = projectStructure.findFolderPathByName('use-cases');
+	const contexts: prompts.Choice[] = [];
+
+	if (pathExists(useCasesFolder)) {
+		const folders = readDirectory(useCasesFolder);
+		folders.forEach((folder) => {
+			const contents = readDirectory(`${useCasesFolder}/${folder}`);
+			const hasFiles = contents.some((content) => content.includes('.'));
+			if (!hasFiles) {
+				contexts.push({ title: folder, value: folder });
+			}
+		});
+	}
+
+	return contexts;
+};
 
 const questions: prompts.PromptObject[] = [
 	{
 		type: 'text',
 		name: 'name',
-		message: 'What is the name of the use case?'
+		message: 'What is the name of the use case?',
 	},
 	{
 		type: 'toggle',
@@ -16,11 +37,6 @@ const questions: prompts.PromptObject[] = [
 		active: 'yes',
 		inactive: 'no',
 	},
-	{
-		type: prev => prev ? 'text' : null,
-		name: 'contextName',
-		message: 'What is the context name?',
-	}
 ];
 
 export default program
@@ -28,14 +44,38 @@ export default program
 	.description('Creates a new use case')
 	.option('-t, --tests', 'Create tests file')
 	.action(async (options) => {
-		const { name, useContext, contextName } = await prompts(questions);
-		const createUseCase = new CreateUseCase();
+		const { name, useContext } = await prompts(questions);
+
+		let context = undefined;
+		if (useContext) {
+			const currentContexts = await getContexts();
+			let contextQuestion: prompts.PromptObject | undefined;
+
+			if (currentContexts.length > 0) {
+				contextQuestion = {
+					type: 'select',
+					name: 'contextName',
+					message: 'Select the context:',
+					choices: currentContexts,
+				};
+			} else {
+				contextQuestion = {
+					type: 'text',
+					name: 'contextName',
+					message: 'What is the context name?',
+				};
+			}
+
+			const { contextName } = await prompts([contextQuestion]);
+			context = contextName;
+		}
 
 		try {
+			const createUseCase = new CreateUseCase();
 			await createUseCase.run({
 				name,
 				useContext,
-				contextName,
+				contextName: context,
 				createTests: options.tests,
 			});
 		} catch (error) {
